e8049768466694f06a53e88aa1a2cab3af89b32f
mjunaid6 <mohmmadjunaid007@gmail.com>
2025-04-30 21:20:47 +0530

    Some minor changes in UI

--- frontend/src/Components/Start.jsx
+++ frontend/src/Components/Start.jsx
@@ -31,39 +31,35 @@ const Start = () => {
 
   return (
     <>
-      <div className="header bg-gradient-to-br from-sky-600 to-cyan-500 p-4">
-        <div className="div flex items-center justify-center">
-          <div className="flex-shrink-0 h-20">
-            {/* Image container */}
-            <img src={logo} className="object-cover h-full" alt="Logo" />
-          </div>
-          <h1 className="font-extrabold">EMPLOYEE360 LOGIN PANEL</h1>
-        </div>
-    
+     <div className="header bg-gradient-to-br from-sky-600 to-cyan-500 p-4">
+     <div className="div flex items-center justify-center">
+        <h1 className="font-extrabold">EMPLOYEEE MANAGMENT LOGIN PANEL</h1>
+     </div>
+      {/* </div> */}
 
-      <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-sky-600 to-cyan-500 p-4">
-        <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-sm text-center h-fit">
-          <h2 className="text-2xl font-bold text-gray-800 m-2">Login As</h2>
-
-          <div className="flex justify-between gap-4 mt-5">
-            <button
-              type="button"
-              onClick={() => navigate("/employee_login")}
-              className="flex-1 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-md transition"
-            >
-              Employee
-            </button>
-            <button
-              type="button"
-              onClick={() => navigate("/adminlogin")}
-              className="flex-1 bg-green-500 hover:bg-green-600 text-white font-semibold py-2 rounded-md transition"
-            >
-              Admin
-            </button>
-          </div>
+    <div className="min-h-screen flex  flex-col  items-center justify-center bg-gradient-to-br from-sky-600 to-cyan-500 p-4">
+      <div className="bg-white p-8 rounded-xl shadow-lg w-full max-w-sm text-center h-24">
+        <h2 className="text-2xl font-bold text-gray-800 m-2">Login As</h2>
+        
+        <div className="flex justify-between gap-4  mt-5">
+          <button
+            type="button"
+            onClick={() => navigate("/employee_login")}
+            className="flex-1 bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 rounded-md transition"
+          >
+            Employee
+          </button>
+          <button
+            type="button"
+            onClick={() => navigate("/adminlogin")}
+            className="flex-1 bg-green-500 hover:bg-green-600 text-white font-semibold py-2 rounded-md transition"
+          >
+            Admin
+          </button>
         </div>
       </div>
-      </div>
+    </div>
+    </div>
     </>
   );
 };
